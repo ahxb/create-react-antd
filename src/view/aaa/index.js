696c2c2bd7f7a7757e83dd0d8662108a9c55b8ce
leo-yang <ahxb521@163.com>
2017-11-27 21:39:55 +0800

    1111

--- src/view/aaa/index.js
+++ src/view/aaa/index.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 
 import {Layout, Menu, Breadcrumb, Icon} from 'antd';
-const {SubMenu} = Menu;
-const {Header, Content, Sider} = Layout;
+
 
 
 class App extends Component {
